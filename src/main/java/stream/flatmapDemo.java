609cd5db962193453403ef1b9dd4d5992b528fae
CodyTseng <cody81314@gmail.com>
2017-10-26 07:52:11 +0800

    Create flatmap demo

---
+++ src/main/java/stream/flatmapDemo.java
@@ -0,0 +1,19 @@
+package stream;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Stream;
+
+/**
+ * Created by cody81314 on 2017/10/26.
+ */
+public class flatmapDemo {
+
+    public static void main(String[] args) {
+        List<String> words = Arrays.asList("Hello", "World");
+        Stream<String> chars = words.stream()
+                .flatMap(word -> Arrays.stream(word.split("")));
+
+        chars.forEach(System.out::println);
+    }
+}
